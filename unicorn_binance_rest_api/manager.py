a03e16b5ecc74248e358c14b66fb6c9b9f7cc981
Oliver Zehentleitner <47597331+oliver-zehentleitner@users.noreply.github.com>
2023-05-05 17:21:52 +0200

    Update manager.py

--- unicorn_binance_rest_api/manager.py
+++ unicorn_binance_rest_api/manager.py
@@ -201,7 +201,7 @@ class BinanceRestApiManager(object):
                  ):
 
         self.name = "unicorn-binance-rest-api"
-        self.version = "1.9.0.dev"
+        self.version = "1.10.0"
         logger.info(f"New instance of {self.get_user_agent()} on {str(platform.system())} {str(platform.release())} "
                     f"for exchange {exchange} started ...")
         if disable_colorama is not True:
