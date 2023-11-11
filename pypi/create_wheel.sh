a4ab14d091f7d2d0f4ea6358cf6e0ccbc7907ce3
Oliver <oliver@zehentleitner.co>
2023-11-11 16:08:16 +0100

    pyproject.toml

--- pypi/create_wheel.sh
+++ pypi/create_wheel.sh
@@ -39,7 +39,7 @@
 
 
 security-check() {
-    echo -n "Did you change the version in \`build_wheels.yml\`, \`CHANGELOG.md\`, \`sphinx/source/conf.py\`, \`setup.py\` and \`/manager.py\`? [yes|NO] "
+    echo -n "Did you change the version in \`build_wheels.yml\`, \`CHANGELOG.md\`, \`sphinx/source/conf.py\`, \`setup.py\`, \`pyproject.toml\` and \`/manager.py\`? [yes|NO] "
     local SURE
     read SURE
     if [ "$SURE" != "yes" ]; then
