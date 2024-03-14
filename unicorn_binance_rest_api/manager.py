dde9802b725f17f6f2ceaca09ce355bc15fa1ebb
Oliver <oliver@zehentleitner.co>
2024-03-14 15:17:00 +0100

    dev version

--- unicorn_binance_rest_api/manager.py
+++ unicorn_binance_rest_api/manager.py
@@ -58,7 +58,7 @@ import time
 
 
 __app_name__: str = "unicorn-binance-rest-api"
-__version__: str = "2.2.1"
+__version__: str = "2.2.1.dev"
 
 logger = logging.getLogger("unicorn_binance_rest_api")
 
